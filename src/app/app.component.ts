ac0a93d1992b2867e75603748d503184ea48519b
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-18 12:17:37 +0530

    added GSAP Scroll Trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Landing-page-Angular-GSAP';
+
+export class AppComponent implements AfterViewInit {
+ 
+  initSrollTrigger():void{
+    gsap.to('.menu, .nav-list, .bottom', {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: 'section.home',
+        scrub: true,
+        start: '60% 60%',
+        end: '70% 30%'
+      }
+    })
+    gsap.to('.heading-main',{
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger:{
+        trigger: 'section.one',
+        scrub: true,
+        start: '90% 80%',
+        end: '100% 20%'
+      }
+    })
+    gsap.to('figure.preview', {
+      scrollTrigger: {
+        trigger: 'figure.preview',
+        toggleClass: 'active',
+        start: '50% 80%',
+        end: '85% 20%'
+      }
+    })
+  }
+
+ngAfterViewInit(): void {
+    this.initSrollTrigger()
+}
+
 }
